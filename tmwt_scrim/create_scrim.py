59731889a2623729c0b14ab1065d45548d4d66b9
Nixotica <nixotica@gmail.com>
2023-02-07 22:43:48 -0800

    Made TMWT Scrimmage and saved the payloads and post urls, next step to create interface to quickly create

---
+++ tmwt_scrim/create_scrim.py
@@ -0,0 +1,2 @@
+teams_pastebin_post_url = "https://competition.trackmania.nadeo.club/api/competitions/4825/teams"
+add_team_to_match_post_url = "https://competition.trackmania.nadeo.club/api/matches/48214/add-team"
